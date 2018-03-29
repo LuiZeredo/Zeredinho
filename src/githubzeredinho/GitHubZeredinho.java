ff3f17c1c5da4dd797eb83f8cc1c1e4d3fb9a87e
IFSC <IFSC@CDR-770622.CEFETSC>
2018-03-28 21:39:15 -0300

    Alteração 28-03-2018  21:36

--- src/githubzeredinho/GitHubZeredinho.java
+++ src/githubzeredinho/GitHubZeredinho.java
@@ -1,15 +1,35 @@
 package githubzeredinho;
+
+import githubzeredinho.Vetor;
+import java.util.Scanner;
+
+
 public class GitHubZeredinho {
-    public static void main(String[] args) {
-    Vetor v = new Vetor(10);
-    Vetor v2 = new Vetor(5);
-    
-    v.randomFill();
-    v2.randomFill();
-    v2 = v2.Concat(v);
-    v2.bubbleSort();
-    System.out.println(v2.toString());
+
+     public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int tam;
+        int posicao;
+        int value;
+        System.out.println("Digite o tamanho do seu vetor: ");
+        tam = ler.nextInt();
+        Vetor vet = new Vetor(tam);
+        Vetor vet2 = new Vetor(tam);
+        vet.randomFill();
+        vet2.randomFill();
+        vet.bubbleSort();
+        vet.toString();
+        
+        System.out.println("Digite a posição do seu vetor e o valor que deseja modificar: ");
+        posicao = ler.nextInt();
+        value = ler.nextInt();
+        
+        vet.setAt(posicao, value);
+        vet.valueAt(posicao);
+        vet.equals(vet2);
+        vet.length();
+        
+        
     }
-   
     
 }
--- src/githubzeredinho/Vetor.java
+++ src/githubzeredinho/Vetor.java
@@ -3,47 +3,96 @@ package githubzeredinho;
 import java.util.Random;
 
 public class Vetor {
-    public Vetor(int size){
-        int 
+    private int vet[];
+    private int vetConcat[];
+    private int size;
+    private int cont=0;
+    private boolean igual = false;
+    private int i=0;
+    private int tamVet;
+    
+   
+    public Vetor(int tam){
+        vet = new int[tam];
+        size = tam;
     }
-    public void randomFill(Vetor v){
-        int i;
-        int a=v.length();
-        Random r=new Random();
-           for(i=0;a>=i;i++)  
-           {  
-              v[i] =r.nextInt(100);
-          }  
+    
+  
+    public void randomFill(){
+        Random rand = new Random();
+        for(cont =0 ; cont < size ; cont ++){
+            vet[cont] = 2 + rand.nextInt();
+        }
     }
+    
+    
     public void bubbleSort(){
-    
+        int troca;
+        int aux;
+        troca = 1;
+        while (troca == 1)
+        {
+            troca = 0;        
+            for (int i = 0; i <= 3; i++)
+            {
+                if (vet[i] > vet[i + 1])
+                {
+                    troca = 1;
+                    aux = vet[i];
+                    vet[i] = vet[i + 1];
+                    vet[i + 1] = aux;
+                }
+            }
+        }
     }
+    
+   
     public String toString(){
-    
-        return null;
+        String vetores[] = new String[size];
+        for(cont=0; cont<size;cont++){
+            vetores[cont] = Integer.toString(vet[cont]) ;
+        }
+        return vetores[size];
     }
+    
+  
     public boolean equals(int[] v){
-        
-        return false;
+        for(cont=0; cont<size;cont++){
+            if(vet[cont] == v[cont]){
+                i++;
+            }
+        }
+        if(i == size){
+            igual = true;
+        }
+        return igual;
     }
     
-    public void setAt(int pos, int value){
-        
+   
+    public void setAt(int posicao, int value1){
+        vet[posicao] = value1;
     }
     
-    public int valueAt(int pos){
-        
-        return 0; 
+
+    public int valueAt(int posicao){
+        return vet[posicao];
     }
+    
     
     public int length(){
-        
-        return 0;
+        tamVet = vet.length;
+        return tamVet;
     }
     
-    public Vetor Concat(Vetor v){
-        
-        return null;   
+  
+    public int Concat(Vetor v){
+        for(cont=0;cont<(size + v.length()); cont ++){
+            if(cont<size){
+                vetConcat[cont] = vet[cont];
+            }else{
+                vetConcat[cont] = v[cont];
+            }
+        }
+        return vetConcat[cont];
     }
-
 }
